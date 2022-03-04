c16432b0b91e58d57e4e20f0e0844e1d01ee9a08
ekankam <eb.kankam26@gmail.com>
2022-03-03 20:05:12 -0600

    Added a reuseable component.

--- src/pages/Stories.jsx
+++ src/pages/Stories.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
+import Row from '../components/Row'
 
 const Stories = () => {
-  return (
-    <section className="stories">
-      <h2>stories</h2>
-    </section>
-  )
+  return <section className="stories"></section>
 }
 
 export default Stories
